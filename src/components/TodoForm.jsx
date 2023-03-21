a4ad5b6ce0bbd9e70cd916ebdbc20bb69d8568a3
Eric Hernandez <ehlabrada@gmail.com>
2023-03-21 13:52:22 -0400

    Add due date input to the form

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,26 +1,42 @@
-import {Input, InputGroup, InputLeftElement} from "@chakra-ui/react";
-import {BsSearch} from "react-icons/bs";
-import {useRef} from "react";
+import {
+    Input,
+    InputGroup,
+    InputLeftElement,
+    InputRightElement, Text,
+} from "@chakra-ui/react";
+import { BsSearch } from "react-icons/bs";
+import { useRef } from "react";
+import { CalendarIcon } from "@chakra-ui/icons";
 
-const TodoForm = ({onAdd}) => {
-    const inputRef = useRef(null)
-    return (
-        <form onSubmit={(event) => {
-            event.preventDefault();
-            if (inputRef.current.value) onAdd(inputRef.current.value)
-        }}>
-            <InputGroup >
-                <InputLeftElement children={< BsSearch/>}/>
-                <Input
-                    ref={inputRef}
-                    width='100%'
-                    borderRadius={5}
-                    variant='filled'
-                    placeholder='Add a task...'
-                />
-            </InputGroup>
-        </form>
-    );
+const TodoForm = ({ onAdd }) => {
+  const inputRef = useRef(null);
+  return (
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        if (inputRef.current.value) onAdd(inputRef.current.value);
+      }}
+    >
+      <InputGroup marginBottom={1}>
+        <InputLeftElement children={<BsSearch />} />
+        <Input
+          ref={inputRef}
+          width="100%"
+          borderRadius={5}
+          variant="filled"
+          placeholder="Add a task..."
+        />
+      </InputGroup>
+        <Text color="gray.500">Due date</Text>
+        <Input
+            placeholder="Due date"
+            size="md"
+            type="date"
+        />
+
+
+    </form>
+  );
 };
 
 export default TodoForm;
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,16 +1,23 @@
-import {useRef} from "react";
-import {Checkbox, HStack, Text, Wrap} from "@chakra-ui/react";
+import { useRef } from "react";
+import { Checkbox, HStack, Text, Wrap } from "@chakra-ui/react";
 import TodoOptions from "./TodoOptions.jsx";
 
 const TodoItem = ({ todo, onDelete, onComplete, onPin }) => {
   return (
     <HStack marginBottom={4} justifyContent="space-between">
       <Wrap>
-        <Checkbox onChange={() => onComplete(todo.id)} isChecked={todo.isCompleted}></Checkbox>
-        <Text>{todo.description}</Text>
+        <Checkbox
+          onChange={() => onComplete(todo.id)}
+          isChecked={todo.isCompleted}
+        ></Checkbox>
+        <Text
+          color={todo.dueDate.getTime() > todo.creationDate.getTime() ? "red" : ""}
+        >
+          {todo.description}
+        </Text>
       </Wrap>
 
-        <TodoOptions todo={todo} onDelete={onDelete} onPin={onPin} />
+      <TodoOptions todo={todo} onDelete={onDelete} onPin={onPin} />
     </HStack>
   );
 };
